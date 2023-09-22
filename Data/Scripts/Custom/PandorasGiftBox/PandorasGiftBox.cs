5cbe6b3f4d79658e03803f55e09bbfbb642b74c8
Nephtan <nephtan@gmail.com>
2023-09-21 22:21:07 -0500

    Create PandorasGiftBox.cs
    Author Norman Lancaster - https://www.servuo.com/archive/pandoras-gift-box.582/
    
    Usage
    Use the [add command to create a PandorasGiftBox anywhere in the world. This is a container that is only accessible by Game Masters and above. It is not moveable, but is visible. I recommend creating them somewhere only staff can access.
    
    Each gift box represents one gift that players may receive on login. Everything contained within the gift box will be duplicated and placed in the player's inventory or, if space is not available, their bank box. Gifting is subject to several conditions:
    
    1. If the gift box is disabled (the default state) or empty, nothing will occur.
    2. If the gift box is set to one gift per account (the default) a gift will be given once to the first character that logs into that account only.
    3. If the gift box is not set to one gift per account, a gift will be given once to every character that logs in.
    
    Configuration
    Place items that are intended to be given as gifts into the gift box. These will be placed directly into players' packs. If it is desired to have the gifts within a container the container should be first created, the gifts placed inside, and the container placed inside the gift box. Try [add RandomGiftBox (included with this package) for some fun.
    
    If it is desirable to give the gift to every character rather than once per account, use the [props command on the gift box and set the OnePerAccount property to false.
    
    To enable giving of gifts from the box, use the "Activate this item" context menu item. To disable giving of gifts, use the "Deactivate this item" context menu. In this way limited-time gifts can be given without the need to permanently delete the original gift set.
    
    Notes
    Each gift box retains the data of which mobiles and accounts have received a gift from the box. If the box is deleted, the records of which mobiles have received the gift is lost. Creating a new gift box with the same contents will give a duplicate gift to those who already received one.
    
    The nature of data retention can also be exploited. For instance, to implement a system whereby the set of gifts given to new accounts is changed every three months, simply remove the contents of the "new account" box and replace it with something new. That way pre-existing accounts will not receive the gift, but new accounts will.

---
+++ Data/Scripts/Custom/PandorasGiftBox/PandorasGiftBox.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Server;
+using Server.ContextMenus;
+using Server.Items;
+using Server.Accounting;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace Server.Items
+{
+    public class RandomGiftBox : BaseContainer
+    {
+        [Constructable]
+        public RandomGiftBox()
+            : base(Utility.RandomDouble() > 0.75d ? Utility.Random(0x232A, 2) : Utility.Random(0x46A2, 6))
+        {
+            SetHue();
+        }
+
+        public RandomGiftBox(Serial serial)
+            : base(serial)
+        { }
+
+        private void SetHue()
+        {
+            if (ItemID <= 0x232B)
+                Hue = GiftBoxHues.RandomNeonBoxHue;
+            else
+                Hue = GiftBoxHues.RandomGiftBoxHue;
+        }
+
+        public override int DefaultGumpID
+        {
+            get
+            {
+                switch (ItemID)
+                {
+                    case 0x232A: return 0x11B;
+                    case 0x232B: return 0x11B;
+                    case 0x46A2: return 0x11B;
+                    case 0x46A3: return 0x11C;
+                    case 0x46A4: return 0x11D;
+                    case 0x46A5: return 0x11E;
+                    case 0x46A6: return 0x11E;
+                    default: return 0x11F;
+                }
+            }
+        }
+
+        public override void OnAfterDuped(Item newItem)
+        {
+            RandomGiftBox that = (RandomGiftBox)newItem;
+            that.ItemID = Utility.RandomDouble() > 0.75d ? Utility.Random(0x232A, 2) : Utility.Random(0x46A2, 6);
+            that.SetHue();
+            that.InvalidateProperties();
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write(0); // Version
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            reader.ReadInt(); // Version
+        }
+    }
+}
+
+namespace Server.Customs
+{
+    public class PandorasGiftBox : RandomGiftBox
+    {
+        private bool m_OnePerAccount = true;
+        private bool m_Active = false;
+        private string m_Message = null;
+        private List<Mobile> m_MobilesGivenTo = new List<Mobile>();
+        private List<string> m_AccountsGivenTo = new List<string>();
+
+        [CommandProperty(AccessLevel.GameMaster)]
+        public bool OnePerAccount
+        {
+            get { return m_OnePerAccount; }
+            set
+            {
+                m_OnePerAccount = value;
+                InvalidateProperties();
+            }
+        }
+
+        [CommandProperty(AccessLevel.GameMaster)]
+        public bool Active
+        {
+            get { return m_Active; }
+            set
+            {
+                m_Active = value;
+                InvalidateProperties();
+            }
+        }
+
+        [CommandProperty(AccessLevel.GameMaster)]
+        public string Message
+        {
+            get { return m_Message; }
+            set
+            {
+                m_Message = value;
+                InvalidateProperties();
+            }
+        }
+
+        [Constructable]
+        public PandorasGiftBox()
+            : base()
+        {
+            Initialize();
+        }
+
+        public PandorasGiftBox(Serial serial)
+            : base(serial)
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.Movable = false;
+            this.Name = "Pandora's Gift Box";
+            EventSink.CharacterCreated += EventSink_CharacterCreated;
+            EventSink.Login += EventSink_Login;
+        }
+
+        public void EventSink_CharacterCreated(CharacterCreatedEventArgs e)
+        {
+            TryGift(e.Mobile);
+        }
+
+        public void EventSink_Login(LoginEventArgs e)
+        {
+            TryGift(e.Mobile);
+        }
+
+        public static void CopyProperties(Item dest, Item src)
+        {
+            PropertyInfo[] props = src.GetType().GetProperties();
+
+            for (int i = 0; i < props.Length; i++)
+            {
+                try
+                {
+                    if (props[i].CanRead && props[i].CanWrite)
+                    {
+                        props[i].SetValue(dest, props[i].GetValue(src, null), null);
+                    }
+                }
+                catch { }
+            }
+        }
+
+        private void Dupe(Server.Items.Container pack, Item copy)
+        {
+            bool done = false;
+            Type t = copy.GetType();
+            ConstructorInfo c = t.GetConstructor(Type.EmptyTypes);
+
+            if (c != null)
+            {
+                try
+                {
+                    object o = c.Invoke(null);
+
+                    if (o != null && o is Item)
+                    {
+                        Item newItem;
+
+                        // Check if the item is a HalloweenBag
+                        if (copy is HalloweenBag)
+                        {
+                            newItem = new HalloweenBag();
+                        }
+                        else
+                        {
+                            newItem = (Item)o;
+                            CopyProperties(newItem, copy);
+                            copy.OnAfterDuped(newItem);
+                        }
+
+                        newItem.Parent = null;
+                        pack.DropItem(newItem);
+                        newItem.InvalidateProperties();
+
+                        if (copy is Server.Items.Container)
+                        {
+                            DupeContainer((Server.Items.Container)copy, (Server.Items.Container)newItem);
+                        }
+                    }
+
+                    done = true;
+                }
+                catch
+                {
+                }
+            }
+
+            if (!done)
+            {
+                Console.WriteLine("Unable to copy an item in a Pandora's Gift Box. All items must have a 0 parameter constructor.");
+            }
+        }
+
+        private void DupeContainer(Server.Items.Container from, Server.Items.Container to)
+        {
+            foreach (Item item in from.Items)
+            {
+                Dupe(to, item);
+            }
+        }
+
+        public void TryGift(Mobile m)
+        {
+            if (m == null || m.Backpack == null || m.BankBox == null ||
+                !m_Active || Items.Count <= 0)
+                return;
+            if (m_OnePerAccount && m_AccountsGivenTo.Contains(m.Account.Username))
+                return;
+            if (!m_OnePerAccount && m_MobilesGivenTo.Contains(m))
+                return;
+
+            if (m.Backpack.CheckHold(m, this, false))
+            {
+                DupeContainer(this, m.Backpack);
+                if (m_Message != null)
+                    m.SendMessage(0x4, m_Message + " A gift has been placed in your backpack.");
+                else
+                    m.SendMessage(0x4, "A gift has been placed in your backpack.");
+            }
+            else
+            {
+                DupeContainer(this, m.BankBox);
+                if (m_Message != null)
+                    m.SendMessage(0x4, m_Message + " A gift has been placed in your bank box.");
+                else
+                    m.SendMessage(0x4, "A gift has been placed in your bank box.");
+            }
+
+            if (m_OnePerAccount)
+            {
+                if (!m_AccountsGivenTo.Contains(m.Account.Username))
+                {
+                    m_AccountsGivenTo.Add(m.Account.Username);
+                }
+            }
+
+            else if (!m_MobilesGivenTo.Contains(m))
+            {
+                m_MobilesGivenTo.Add(m);
+            }
+        }
+
+        public override void OnDoubleClick(Mobile from)
+        {
+            // Only staff can open the box
+            if (from.AccessLevel >= AccessLevel.GameMaster)
+                base.OnDoubleClick(from);
+        }
+
+        public override void AddNameProperty(ObjectPropertyList list)
+        {
+            base.AddNameProperty(list);
+            list.Add(m_OnePerAccount ? "One per Account" : "One per Character");
+            list.Add(m_Active ? "Active" : "Disabled");
+        }
+
+        public override void GetContextMenuEntries(Mobile from, List<ContextMenuEntry> list)
+        {
+            base.GetContextMenuEntries(from, list);
+            if (from.AccessLevel >= AccessLevel.GameMaster)
+            {
+                list.Add(new EnableContextMenuEntry(!m_Active, this));
+            }
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write(0); // Version
+
+            writer.Write(m_OnePerAccount);
+            writer.Write(m_Active);
+            writer.Write(m_Message);
+            writer.WriteMobileList(m_MobilesGivenTo);
+            string[] accounts = m_AccountsGivenTo.ToArray();
+            writer.Write(accounts.Length);
+            foreach (string account in accounts)
+            {
+                writer.Write(account);
+            }
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+
+            switch (version)
+            {
+                case 0:
+                    m_OnePerAccount = reader.ReadBool();
+                    m_Active = reader.ReadBool();
+                    m_Message = reader.ReadString();
+                    m_MobilesGivenTo.Clear();
+                    ArrayList mobileList = reader.ReadMobileList();
+                    m_MobilesGivenTo.Clear();
+                    foreach (Mobile mob in mobileList)
+                    {
+                        m_MobilesGivenTo.Add(mob);
+                    }
+                    m_AccountsGivenTo.Clear();
+                    int count = reader.ReadInt();
+                    for (int i = 0; i < count; ++i)
+                    {
+                        m_AccountsGivenTo.Add(reader.ReadString());
+                    }
+                    break;
+            }
+        }
+
+        private class EnableContextMenuEntry : ContextMenuEntry
+        {
+            private bool m_Activate;
+            private PandorasGiftBox m_Parent;
+            public EnableContextMenuEntry(bool activate, PandorasGiftBox parent)
+                : base(activate ? 1011034 : 1011035)
+            {
+                m_Activate = activate;
+                m_Parent = parent;
+            }
+            public override void OnClick()
+            {
+                m_Parent.Active = m_Activate;
+            }
+        }
+    }
+}
