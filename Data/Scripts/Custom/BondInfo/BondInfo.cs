ce0749b2437f480d8b2452cdc5ae155d8b33c802
lgr-eng <140429203+lgr-eng@users.noreply.github.com>
2023-10-03 22:33:57 -0300

    Update BondInfo.cs
    added check to see if pet is already bonded to prevent negative numbers, and some small improvements on code

--- Data/Scripts/Custom/BondInfo/BondInfo.cs
+++ Data/Scripts/Custom/BondInfo/BondInfo.cs
@@ -7,58 +7,70 @@ using Server.Network;
 
 namespace Server.Commands
 {
-  public class BondInfoCommand
+	public class BondInfoCommand
 	{
 		public static void Initialize()
-			{
-	       CommandSystem.Register( "BondInfo", AccessLevel.Player, new CommandEventHandler( BondInfo_OnCommand ) );
-			}
+		{
+			CommandSystem.Register("BondInfo", AccessLevel.Player, new CommandEventHandler(BondInfo_OnCommand));
+		}
 			
-		[Usage( "BondInfo" )]
-		[Description( "Tells you how much time remaining until your pet can bond." )]
-		public static void BondInfo_OnCommand( CommandEventArgs e )
+		[Usage("BondInfo")]
+		[Description("Tells you how much time remaining until your pet can bond.")]
+		public static void BondInfo_OnCommand(CommandEventArgs e)
 		{
 			Mobile from = e.Mobile;
-			e.Mobile.BeginTarget( -1, false, TargetFlags.None, new TargetCallback( BondInfo_OnTarget ) );
+			e.Mobile.BeginTarget(-1, false, TargetFlags.None, new TargetCallback(BondInfo_OnTarget));
 			e.Mobile.SendMessage("Target the pet you wish to know the bonding timer of");
 		}	
 		
 		
-		public static void BondInfo_OnTarget( Mobile from, object targeted )	
+		public static void BondInfo_OnTarget(Mobile from, object targeted)	
 		{	
-			if ( targeted is BaseCreature )
-			 {
-				 BaseCreature targ = (BaseCreature)targeted;		
-						if ( targ.ControlMaster == from ) 
-							{
-								if( targ.BondingBegin == DateTime.MinValue )
-								{
-									from.SendMessage("Your pet hasn't started to bond yet, please feed it and try again." );
-								}
-						else	
-						{
-								DateTime today = DateTime.UtcNow;
-								DateTime willbebonded = targ.BondingBegin.AddDays(7);
-								TimeSpan bondedfor = targ.BondingBegin - today;
-								TimeSpan daystobond = willbebonded - today;
-								string BondInfo = string.Format("The pet started bonding with you at {0}. Its {1} days, {2} hours and {3} minutes until it bonds", targ.BondingBegin, daystobond.Days, 
-								daystobond.Hours, daystobond.Minutes  );
-								from.SendMessage ( BondInfo );
-						}		
+			if (targeted is BaseCreature)
+			{
+				BaseCreature targ = (BaseCreature)targeted;		
+				if (targ.ControlMaster == from)
+                {
+                    if (targ.IsBonded == true)
+                    {
+                        from.SendMessage("Bonded");
+                    }
+                    else if (targ.BondingBegin == DateTime.MinValue)
+					{
+                        from.SendMessage("Your pet hasn't started to bond yet, please feed it and try again.");
+                    }
+					else	
+					{
+						string BondInfo;
+                        DateTime today = DateTime.Now;
+						DateTime willbebonded = targ.BondingBegin + targ.BondingDelay;
+						TimeSpan daystobond = willbebonded - today;
+                        if ((daystobond.Days > 0) || (daystobond.Hours > 0) || (daystobond.Minutes > 0))
+                        {
+                            BondInfo = string.Format("The pet started bonding with you at {0}. Its {1} days, {2} hours and {3} minutes until it bonds", targ.BondingBegin, daystobond.Days,
+                            daystobond.Hours, daystobond.Minutes);
+                        }
+                        else
+                        {
+                            BondInfo = "Ready to bond!";
+                        }
+
+                        from.SendMessage ( BondInfo );
+                    }		
 							
-			}
-			else
+				}
+				else
 				{ 
-          from.BeginTarget( -1, false, TargetFlags.None, new TargetCallback( BondInfo_OnTarget ) );
-          from.SendMessage( "That is not your pet!" ); 
-        } 	
+					from.BeginTarget( -1, false, TargetFlags.None, new TargetCallback( BondInfo_OnTarget ) );
+					from.SendMessage( "That is not your pet!" ); 
+				} 	
 			
-		}			
- 		else
+			}			
+ 			else
 			{
 				from.BeginTarget( -1, false, TargetFlags.None, new TargetCallback( BondInfo_OnTarget ) );
-			 	from.SendMessage("That is not a pet!" ); 
-		 }
+				from.SendMessage("That is not a pet!" ); 
+			}
+		}
 	}
- }
 }
