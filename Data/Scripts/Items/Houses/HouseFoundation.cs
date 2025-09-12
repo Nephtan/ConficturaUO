01647a2ba1fdce7e534d7654973b94dae47f8dd6
Nephtan <121361384+Nephtan@users.noreply.github.com>
2025-09-12 16:45:11 -0500

    Adjustments for house borders

--- Data/Scripts/Items/Houses/HouseFoundation.cs
+++ Data/Scripts/Items/Houses/HouseFoundation.cs
@@ -736,8 +736,8 @@ namespace Server.Multis
                     new Rectangle2D(
                         mcl.Min.X + 1,
                         mcl.Min.Y + 1,
-                        mcl.Width - 2,
-                        mcl.Height - 2
+                        mcl.Width - 1,
+                        mcl.Height - 1
                     )
                 };
             }
