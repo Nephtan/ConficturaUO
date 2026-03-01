1e0fd38b6d78c272db664aab74816cb64ddc6466
Nephtan <nephtan@gmail.com>
2026-03-01 07:59:39 -0600

    Revert "Update AutoSave.cs"
    This reverts commit c1c557199996df0a669734803c7fadd9f2c60c1a.

--- Data/Scripts/System/Misc/AutoSave.cs
+++ Data/Scripts/System/Misc/AutoSave.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Threading.Tasks;
 using Server;
 using Server.Commands;
 using Server.Items;
@@ -145,23 +144,16 @@ namespace Server.Misc
 
             World.WaitForWriteCompletion();
 
+            try
+            {
+                Backup();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("WARNING: Automatic backup FAILED: {0}", e);
+            }
+
             World.Save(true, permitBackgroundWrite);
-
-            World.WaitForWriteCompletion();
-
-            Task.Run(
-                () =>
-                {
-                    try
-                    {
-                        Backup();
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("WARNING: Automatic backup FAILED: {0}", e);
-                    }
-                }
-            );
         }
 
         private static string[] m_Backups = new string[]
